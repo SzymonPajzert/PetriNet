77dcebf5776eb5a10cb94eb8a92f04491b8e1187
Szymon Pajzert <szymonpajzert@gmail.com>
2017-03-11 22:31:53 +0100

    Little changes

--- src/main/scala/petrify/spark/PetriNetExplorer.scala
+++ src/main/scala/petrify/spark/PetriNetExplorer.scala
@@ -8,17 +8,17 @@ import petrify.model.{PetriNet, Place, State, Transition}
   * Created by szymonpajzert on 27.12.16.
   */
 object PetriNetExplorer {
-  def lookForHelper(petriNet: PetriNet, pastStates: RDD[State], toIterate: RDD[State], end: State):Boolean = {
+  def lookForHelper(petriNet: PetriNet, pastStates: RDD[State], toIterate: RDD[State], end: State => Boolean):Boolean = {
     val nextStates = toIterate.flatMap(petriNet.iterate).subtract(pastStates).persist()
     if (nextStates.isEmpty()) false else {
-      if(nextStates.filter(_ == end).count() > 0) true else {
+      ((nextStates filter end).count() > 0) || {
         val newPastStates = pastStates union nextStates
         lookForHelper(petriNet, newPastStates, nextStates, end)
       }
     }
   }
 
-  def lookFor(sc: SparkContext, petriNet: PetriNet, start: State, end: State):Boolean = {
+  def lookFor(sc: SparkContext, petriNet: PetriNet, start: State, end: State => Boolean):Boolean = {
     val initial = sc.parallelize(Seq(start))
     lookForHelper(petriNet, sc.parallelize(Seq()), initial, end)
   }
